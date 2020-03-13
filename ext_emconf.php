1e26a777a0e0e5fe141316db830656c153e071f4
Alexander Stehlik <alexander.stehlik@gmail.com>
2020-03-13 14:36:22 +0100

    [CLEANUP] Beautify extbase dependency in ext_emconf.php

--- ext_emconf.php
+++ ext_emconf.php
@@ -13,7 +13,7 @@ $EM_CONF[$_EXTKEY] = [
     'constraints' => [
         'depends' => [
             'typo3' => '9.5.0-9.5.99',
-            'extbase' => '0.0.0-99.99.99',
+            'extbase' => '9.5.0-9.5.99',
         ],
         'conflicts' => [],
         'suggests' => [],
