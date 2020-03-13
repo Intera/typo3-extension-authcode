b536b3f1c6850f60a304980ed1a59de9940df68e
Alexander Stehlik <alexander.stehlik@gmail.com>
2020-03-13 11:28:40 +0100

    [TASK] Improve compatiblity constraints
    Current version officially support TYPO3 6, 7 and 8 and
    PHP 5.6 up to 7.1 (same as nimut/testing-framework).

--- ext_emconf.php
+++ ext_emconf.php
@@ -1,29 +1,21 @@
 <?php
 
-/***************************************************************
- * Extension Manager/Repository config file for ext "authcode".
- *
- * Auto generated 04-07-2013 17:03
- *
- * Manual updates:
- * Only the data in the array - everything else is removed by next
- * writing. "version" and "dependencies" must not be touched!
- ***************************************************************/
-
-$EM_CONF[$_EXTKEY] = array(
-	'title' => 'Auth code libraries',
-	'description' => 'Library for generating and validating one time authorization codes (e.g. for email validation).',
-	'category' => 'misc',
-	'version' => '0.2.1',
-	'state' => 'beta',
-	'author' => 'Alexander Stehlik',
-	'author_email' => 'astehlik@intera.de',
-	'author_company' => 'Intera GmbH',
-	'constraints' => array(
-		'depends' => array(
-			'typo3' => '6.2.2-7.6.99',
-		),
-		'conflicts' => array(),
-		'suggests' => array(),
-	),
-);
+/** @var string $_EXTKEY */
+$EM_CONF[$_EXTKEY] = [
+    'title' => 'Auth code libraries',
+    'description' => 'Library for generating and validating one time authorization codes (e.g. for email validation).',
+    'category' => 'misc',
+    'version' => '0.3.0-dev',
+    'state' => 'beta',
+    'author' => 'Alexander Stehlik',
+    'author_email' => 'astehlik@intera.de',
+    'author_company' => 'Intera GmbH',
+    'constraints' => [
+        'depends' => [
+            'typo3' => '6.2.2-8.7.99',
+            'extbase' => '0.0.0-99.99.99'
+        ],
+        'conflicts' => [],
+        'suggests' => [],
+    ],
+];
